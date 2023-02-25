1c86f894815dfde9b164cc086dd1cbb88bd5863d
vsalgueiro7 <vitorsalgueiro@outlook.com>
2023-02-25 15:51:13 -0300

    att

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import './shared/services/translationsYup';
 import { router } from './routes';
 
 const server = express();
-
+server.use(cors({
+    origin: process.env.ENABLED_CORS?.split(';') || []
+  }));
 server.use(express.json());
 server.use(router);
 
